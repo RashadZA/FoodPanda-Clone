4d13cf08a82140687458d8a14eca74535f96eaa9
RashadZA <97411218+RashadZA@users.noreply.github.com>
2023-08-14 13:05:52 +0600

    Setup Firebase with project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:foodpanda_clone/Services/Routes/app_pages.dart';
 import 'package:foodpanda_clone/Utils/design_utils.dart';
+import 'package:foodpanda_clone/firebase_options.dart';
 import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
